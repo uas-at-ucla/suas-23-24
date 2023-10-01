1110abbf627fd275ad4abfcff275013aef820996
Nathan Chan <nathanchan631@gmail.com>
2023-10-01 16:00:10 -0700

    initial commit. docker tests, vision server, and github actions

---
+++ vision/test.py
@@ -0,0 +1,19 @@
+import unittest
+
+# Example test:
+#
+# from odlc import my_script
+
+# class MyScriptTests(unittest.TestCase):
+# 	image_path_1 = '/app/images/image1.png'
+# 	image_path_2 = '/app/images/image2.png'
+
+# 	def test_my_script_1(self):
+# 		self.assertEqual(my_script.get_detection(image_path_1), "A")
+
+# 	def test_my_script_2(self):
+# 		self.assertEqual(my_script.get_detection(image_path_2), "B")
+
+
+if __name__ == "__main__":
+    unittest.main()
