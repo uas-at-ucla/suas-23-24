b1d1367a5140b2ff780f922bd8c15e8b5c4a21cc
Kush Agarwal <kushhansagarwal@gmail.com>
2023-10-19 14:49:16 -0700

    added test images

--- vision/odlc/detector.py
+++ vision/odlc/detector.py
@@ -4,6 +4,8 @@ Stateful representation of vision system
 
 import json
 import redis
+import pyexif as exif
+import math
 
 
 r = redis.Redis(host='redis', port=6379, db=0)
@@ -46,3 +48,46 @@ def process_queued_image(img):
 
     json_detections = json.dumps(detections)
     r.set('detector/detections', json_detections)
+
+def calculate_object_gps(image, detections):
+    image_width = image.size[0]
+    image_height = image.size[1]
+
+    image_metadata = exif.ImageMetadata(image)
+    image_metadata.read()
+
+    location = [image_metadata.gps_latitude, image_metadata.gps_longitude]
+    true_heading = image_metadata.gps_img_direction
+    altitude = image_metadata.gps_altitude
+    focal_length = image_metadata.focal_length
+    sensor_width = image_metadata.sensor_width
+
+    center_pixel_x = image_width / 2
+    center_pixel_y = image_height / 2
+
+    GSD = (altitude * sensor_width) / (focal_length * image_width)
+
+    detections_coords = []
+    
+    for detection in detections:
+        x_pos = detection['x_pos']
+        y_pos = detection['y_pos']
+
+        delta_x = (x_pos - center_pixel_x) * GSD
+        delta_y = (y_pos - center_pixel_y) * GSD
+
+        true_x = delta_x * math.cos(true_heading) - delta_y * math.sin(true_heading)
+        true_y = delta_x * math.sin(true_heading) + delta_y * math.cos(true_heading)
+
+        delta_gps_x = true_x / 111111
+        delta_gps_y = true_y / 111111
+
+        detections_coords = [location[0] + delta_gps_x, location[1] + delta_gps_y]
+
+
+
+        
+
+        
+
+
