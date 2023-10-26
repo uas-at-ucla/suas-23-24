d4f828bce02e7895c7d4c1ecb7ae86d6728ecf21
SeanSon2005 <astroson2005@gmail.com>
2023-10-26 02:14:36 -0700

    pass flake and built

--- vision/test.py
+++ vision/test.py
@@ -2,6 +2,7 @@ import unittest
 
 from vision.odlc import shape_color_detection
 
+
 class ShapeColorDetectionTests(unittest.TestCase):
     image_path_1 = "vision/images/test/shape_color_detection_1.jpg"
     image_path_2 = "vision/images/test/shape_color_detection_2.jpg"
@@ -16,6 +17,7 @@ class ShapeColorDetectionTests(unittest.TestCase):
     def test_shape_color_detection_false_positive(self):
         results = shape_color_detection.detect_shape_color(self.image_path_2)
         self.assertEqual(results, [])
+
 
 if __name__ == "__main__":
     unittest.main()
