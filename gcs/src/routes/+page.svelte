882f6c8ab5acf8d5d7a07494005eaee4107a4271
Kush Agarwal <kushhansagarwal@gmail.com>
2023-11-06 17:11:50 -0800

    initialized svelte files

---
+++ gcs/src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { Canvas } from '@threlte/core';
+	import Scene from './Scene.svelte';
+</script>
+
+<section class="p-5">
+	<div class="">
+		<div class="navbar rounded-md bg-primary">
+			<a class="btn btn-ghost normal-case text-xl">UAS@UCLA | Ground Control Station</a>
+		</div>
+	</div>
+
+	<div class="grid grid-cols-4">
+		
+	</div>
+
+	<p class="text-md mt-5 mb-2">Live Orientation</p>
+	<div class="w-96 h-96 bg-base-200 rounded-md">
+		<Canvas>
+			<Scene />
+		</Canvas>
+	</div>
+</section>
