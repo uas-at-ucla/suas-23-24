cb20623aa0b925adefc3019cfe18d3e94258f666
Aiden Wong <93459706+AidenW0ng@users.noreply.github.com>
2023-11-30 14:25:29 -0800

    Fixed small typos throughout the comments

--- flight/examples/usb_camera/stream_camera.py
+++ flight/examples/usb_camera/stream_camera.py
@@ -3,9 +3,10 @@ import subprocess
 import time
 import signal
 
-# This example executes teh "usb_to_camera.py" file
+# This example executes the "usb_to_camera.py" file
 # and uses OpenCV to stream the newly added usb camera
-# to the screen.
+# to the screen. 
+# Only run this file by itself to have the camera running.
 
 # Optionally commented out is the option to take photos
 # from the stream once per frame (capped at 1000).
